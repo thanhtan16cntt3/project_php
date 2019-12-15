692b0290645a23c11633296bf34d21cd04bbb896
Nguyen Thanh Tan <dv.nttan98@gmail.com>
2019-12-15 13:41:51 +0700

    fixed add product

--- MVC/Views/Master_page.php
+++ MVC/Views/Master_page.php
@@ -13,8 +13,10 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
         <!-- Custom styles for this template-->
         <link href="../../Project_web_admin/Public/css/sb-admin-2.min.css" rel="stylesheet">
-        <!-- <link href="../../Project_web_admin/Public/css/dataTables.bootstrap.min.css" rel="stylesheet"> -->
+        <link href="../../Project_web_admin/Public/css/bootstrap.min.css" rel="stylesheet">
         <link href="../../Project_web_admin/Public/css/jquery.dataTables.min.css" rel="stylesheet">
+        <link href="../../Project_web_admin/Public/css/tm-style-new.css" rel="stylesheet">
+        <link href="../../Project_web_admin/Public/css/jquery-ui.min.css" rel="stylesheet">
     </head>
     <body id="page-top">
         <!-- Page Wrapper -->
@@ -47,24 +49,6 @@
         <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
         </a>
-        <!-- Logout Modal-->
-        <!-- <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <a class="btn btn-primary" href="login.html">Logout</a>
-                    </div>
-                </div>
-            </div>
-        </div> -->
         <!-- Bootstrap core JavaScript-->
         <script src="../../Project_web_admin/Public/vendor/jquery/jquery.min.js"></script>
         <script src="../../Project_web_admin/Public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -101,7 +85,10 @@
                         items:4
                     }
                 }
-            })
+            });
         </script>
+
+
+
     </body>
 </html>
--- MVC/Views/inc/side-bar.php
+++ MVC/Views/inc/side-bar.php
@@ -20,7 +20,7 @@
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Custom Products:</h6>
-                <a class="collapse-item" href="">Add Product</a>
+                <a class="collapse-item" href="/Project_web_admin/AddProduct">Add Product</a>
                 <a class="collapse-item" href="/Project_web_admin/Products">Manage Products</a>
             </div>
         </div>
